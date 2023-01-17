550ea149d8e9f2972d6e6c89a9f87f11a15c0991
liangxiaojiang01 <122680672+liangxiaojiang01@users.noreply.github.com>
2023-01-17 16:52:03 +0800

    hot-fix update GitTest.java
    hot-fix pull

--- src/atgit/GitTest.java
+++ src/atgit/GitTest.java
@@ -7,5 +7,6 @@ public class GitTest {
         System.out.println("hello Git2");
         System.out.println("hot-fix test");
         System.out.println("hot-fix push");
+        System.out.println("hot-fix pull");
     }
 }
