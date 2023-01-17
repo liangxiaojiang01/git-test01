1f52a9c00b7078e5cb891b3f542260e1f708dfff
liangxj <3137129132@qq.com>
2023-01-17 16:11:35 +0800

    hot-fix commit1

--- src/atgit/GitTest.java
+++ src/atgit/GitTest.java
@@ -5,6 +5,6 @@ public class GitTest {
         System.out.println("hello Git");
         System.out.println("hello Git1");
         System.out.println("hello Git2");
-        System.out.println("master test");
+        System.out.println("hot-fix test");
     }
 }
