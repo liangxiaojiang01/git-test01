7adb8ccf8946d6ba1d17744c0e1ec896e24d032b
liangxj <3137129132@qq.com>
2023-01-17 17:23:31 +0800

    02 first commit

--- src/atgit/GitTest.java
+++ src/atgit/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("master test");
         System.out.println("hot-fix test");
         System.out.println("master push");
+        System.out.println("master lxj02 commit");
     }
 }
